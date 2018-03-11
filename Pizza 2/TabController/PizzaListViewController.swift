69d7b30c6ba02a102cb35a83b7b78ee6aa1af730
saleh <salehvm@code.edu.az>
2018-03-11 13:02:54 +0400

    fix colours view create Pizzalistviewcontroller in tabbar

--- Pizza 2/TabController/PizzaListViewController.swift
+++ Pizza 2/TabController/PizzaListViewController.swift
@@ -7,17 +7,27 @@
 //
 
 import UIKit
+import PullToMakeSoup
 
-class PizzaListViewController: UIViewController {
+class PizzaListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var addMenu: UIView!
     @IBOutlet weak var normalMenu: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     
     var tapGestureRecognizer = UITapGestureRecognizer()
+    let refresher:PullToMakeSoup = PullToMakeSoup(at: .top)
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        self.tableView.separatorStyle = .none
+        tableView.sectionHeaderHeight = 5.0;
 
         initVars()
     }
@@ -25,6 +35,20 @@ class PizzaListViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //        tableView.addPullToRefresh(refresher) {
+        //
+        //            self.tableView.startRefreshing(at: .top)
+        //            print("pulling now")
+        //        }
+        tableView.addPullToRefresh(PullToMakeSoup(at: .top))  {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                self.tableView.endRefreshing(at: .top)
+            }
+        }
     }
     
     func initVars(){
@@ -39,14 +63,26 @@ class PizzaListViewController: UIViewController {
         self.normalMenu.addGestureRecognizer(tapGestureRecognizer)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.contentView.layoutMargins.bottom = 20
+        
+        cell.textLabel?.text = "\(indexPath.row)"
+  
+        return cell
+        
+    }
 
+    
+    //    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //        if indexPath.row == 0{
+    //            self.tableView.endRefreshing(at: .top)
+    //        }
+    //
+    //    }
 }
